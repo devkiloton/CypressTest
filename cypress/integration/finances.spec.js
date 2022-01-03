c3f7ff324face8dcd71152f5d2bb7c0293e0c06e
devkiloton <dev.kiloton@gmail.com>
2022-01-03 11:41:04 -0300

    Validating results and expected results

--- cypress/integration/finances.spec.js
+++ cypress/integration/finances.spec.js
@@ -1,5 +1,5 @@
 /// <reference types='cypress' />
-
+import { format } from '../support/utils'
 context('Dev Finances Agilizei', () => {
 
     //HOOKS
@@ -63,4 +63,47 @@ context('Dev Finances Agilizei', () => {
 
         cy.get('#data-table tbody tr').should('have.length', 0);
     });
+
+    it('Validar saldo com diversas transações', () => {
+        const entrada = 'Mesada';
+        const saida = 'KinderOvo';
+
+        cy.get('#transaction .button').click();
+        cy.get('#description').type(entrada);
+        cy.get('[name=amount]').type(20);
+        cy.get('[type=date]').type('2022-03-17');
+        cy.get('button').contains('Salvar').click();
+
+        cy.get('#transaction .button').click();
+        cy.get('#description').type(saida);
+        cy.get('[name=amount]').type(-12);
+        cy.get('[type=date]').type('2022-03-17');
+        cy.get('button').contains('Salvar').click();
+
+        let incomes = 0;
+        let expenses = 0;
+
+        cy.get('#data-table tbody tr')
+          .each(($el, index, $list) => {
+              cy.get($el).find('td.income, td.expense').invoke('text').then(text => {
+                if(text.includes('-')){
+                    expenses = expenses + format(text);
+                }
+                else{
+                    incomes = incomes + format(text);
+                }
+
+                cy.log('Entradas', incomes);
+                cy.log('Saidas', expenses);
+              })
+          }
+        );
+        
+        cy.get('#totalDisplay').invoke('text').then(text => {
+          let formattedTotalDisplay = format(text);
+          let expectedTotal = incomes + expenses;
+
+          expect(formattedTotalDisplay).to.eq(expectedTotal);
+        })
+    })
 });
