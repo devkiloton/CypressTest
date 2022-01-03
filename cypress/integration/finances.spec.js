cb0dd25f034c194b205170b71b7efef6c98141d3
devkiloton <dev.kiloton@gmail.com>
2022-01-03 10:08:30 -0300

    testing inputs, outputs, lengths & responsivenes

--- cypress/integration/finances.spec.js
+++ cypress/integration/finances.spec.js
@@ -13,23 +13,54 @@ context('Dev Finances Agilizei', () => {
         cy.get('#data-table tbody tr').should('have.length', 0);
     })
 
-    afterEach(() => {
-        cy.get('#data-table tbody tr').should('have.length', 1);
-    })
-
     it('Cadastrar entradas', () => {
         cy.get('#transaction .button').click();
         cy.get('#description').type('Mesada');
         cy.get('[name=amount]').type(20);
         cy.get('[type=date]').type('2022-03-17');
         cy.get('button').contains('Salvar').click();
+
+        cy.get('#data-table tbody tr').should('have.length', 1);
     });
 
     it('Cadastrar saídas', () => {
         cy.get('#transaction .button').click();
         cy.get('#description').type('Mesada');
+        cy.get('[name=amount]').type(-20);
+        cy.get('[type=date]').type('2022-03-17');
+        cy.get('button').contains('Salvar').click();
+
+        cy.get('#data-table tbody tr').should('have.length', 1);
+    });
+
+    it('Remover entradas e saídas', () => {
+        const entrada = 'Mesada';
+        const saida = 'KinderOvo';
+
+        cy.get('#transaction .button').click();
+        cy.get('#description').type(entrada);
         cy.get('[name=amount]').type(20);
         cy.get('[type=date]').type('2022-03-17');
         cy.get('button').contains('Salvar').click();
+
+        cy.get('#transaction .button').click();
+        cy.get('#description').type(saida);
+        cy.get('[name=amount]').type(-12);
+        cy.get('[type=date]').type('2022-03-17');
+        cy.get('button').contains('Salvar').click();
+
+        cy.get('td.description')
+        .contains(entrada)
+        .parent()
+        .find('img[onclick*=remove]')
+        .click();
+
+        cy.get('td.description')
+        .contains(saida)
+        .parent()
+        .find('img[onclick*=remove]')
+        .click();
+
+        cy.get('#data-table tbody tr').should('have.length', 0);
     });
 });
